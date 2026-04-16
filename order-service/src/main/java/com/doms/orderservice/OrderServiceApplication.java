5aba74e2a0033f05ccc99f1f1dd82d8195a921e0
Your Name <you@example.com>
2026-04-16 23:35:42 +0530

    Fix compilation errors and update GitHub Actions to v4

--- order-service/src/main/java/com/doms/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/doms/orderservice/OrderServiceApplication.java
@@ -2,6 +2,7 @@ package com.doms.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.retry.annotation.EnableRetry;
 
 @EnableRetry
